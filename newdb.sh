40cd835959c8e4c9a810a8710d49744bd1d97ef5
Chris Mendez <mail@chrisaiv.com>
2016-07-31 21:03:07 -0700

    Removed the shell scripts and repalced them with tasks

--- newdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# 1. Drop the db schema
-# 2. Create a new one
-# 3. Migrate the data
-# 4. Populate the data
-rails db:purge db:create db:migrate db:seed --trace
--- rerun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# 1. Run Rails while monitoring /app, /config
-rerun --dir config,app rails s
